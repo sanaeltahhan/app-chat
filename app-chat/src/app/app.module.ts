9f516034a312b6686873b559b6555338708082bb
Robin Ladenburger <ladenburger.robin57@gmail.com>
2020-08-19 12:31:06 +0200

    création navbar

--- app-chat/src/app/app.module.ts
+++ app-chat/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import { Route, RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+const ROUTES: Routes = [
+  // { path: 'accueil', component: },
+  // { path: 'donner', component: },
+  // { path: 'contact', component: },
+  { path: '', pathMatch: 'full', redirectTo: '/accueil' },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
